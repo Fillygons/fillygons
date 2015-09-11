ab26e5a8d54de099315e2cf473ec52610a6e3a17
Michael Schwarz <michi.schwarz@gmail.com>
2015-09-11 22:02:36 +0200

    Compile Asymptote files in separate working dir.
    Asymptote leaves a trail of partially cleaned-up temporary files behind it when PDFLaTeX is used.
    
    With this changes, Asymptote files are compiled in a temporary directory outside the project root.

--- support/asymptote/__main__.py
+++ support/asymptote/__main__.py
@@ -1,16 +1,23 @@
-import os, sys
+import os, sys, shutil
 from lib import util
 
 
-def _asymptote(in_path, out_path, asymptote_dir):
-	util.command([os.environ['ASYMPTOTE'], '-f', 'pdf', '-o', out_path, in_path], set_env = { 'ASYMPTOTE_DIR': asymptote_dir })
+def _asymptote(in_path, out_path, asymptote_dir, working_dir):
+	util.command([os.environ['ASYMPTOTE'], '-f', 'pdf', '-o', out_path, in_path], set_env = { 'ASYMPTOTE_DIR': asymptote_dir }, working_dir = working_dir)
 
 
 def main(in_path, out_path):
 	_, out_suffix = os.path.splitext(out_path)
 	
 	if out_suffix == '.pdf':
-		_asymptote(in_path, out_path, os.path.dirname(in_path))
+		with util.TemporaryDirectory() as temp_dir:
+			absolute_in_path = os.path.abspath(in_path)
+			temp_out_path = os.path.join(temp_dir, 'out.pdf')
+			
+			# Asymptote creates A LOT of temp files (presumably when invoking LaTeX) and leaves some of them behind. Thus we run asymptote in a temporary directory.
+			_asymptote(absolute_in_path, 'out', os.path.dirname(absolute_in_path), temp_dir)
+			
+			shutil.copyfile(temp_out_path, out_path)
 	else:
 		raise Exception('Unknown file type: {}'.format(out_suffix))
 
