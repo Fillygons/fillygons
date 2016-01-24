6c89ae44c1c2e8e1043cfe64762975ae59967b62
Michael Schwarz <michi.schwarz@gmail.com>
2016-01-24 18:19:06 +0100

    Cleaned up Python code to generate tiles.

--- generate_sources.py
+++ generate_sources.py
@@ -1,53 +1,69 @@
-import sys, os, functools, glob
+import sys, os, json, collections
+
+
+symbol = collections.namedtuple('symbol', ['name'])
+
+def serialize_value(value):
+	if isinstance(value, list):
+		return '[{}]'.format(', '.join(map(serialize_value, value)))
+	elif isinstance(value, symbol):
+		return value.name
+	else:
+		return json.dumps(value)
 
 
 def main(path = None):
 	files = { }
 	
-	def generate_file(path, function, *args):
-		files[path] = lambda: function(*args)
-	
-	def code(included_file, function_call):
-		yield 'use <_fillygon.scad>'
+	def generate_file(function : list, includes : list, *name_parts, **arguments):
+		def fn():
+			for type, path in [('use', '_fillygon.scad')] + includes:
+				yield '{} <{}>'.format(type, path)
+			
+			args_str = ['{} = {}'.format(k, serialize_value(v)) for k, v in sorted(arguments.items())]
+			
+			yield 'render() {}({});'.format(function, ', '.join(args_str))
 		
-		if included_file is not None:
-			yield 'include <{}>'.format(included_file)
+		path = 'src/{}.scad'.format('-'.join(name_parts))
 		
-		yield 'render() {};'.format(function_call)
+		assert path not in files
+		
+		files[path] = fn
 	
-	def fillygon(file_name, included_file, called_function, arguments):
-		for i in '0.24', '0.4':
-			generate_file('src/{}-{}mm.scad'.format(file_name, i), code, included_file, '{}({}, gap = {})'.format(called_function, arguments, i))
-			generate_file('src/{}-filled-{}mm.scad'.format(file_name, i), code, included_file, '{}({}, filled = true, gap = {})'.format(called_function, arguments, i)) 
+	def fillygon_gap(function, includes, *name_parts, **arguments):
+		for i in .25, .4:
+			generate_file(function, includes, *name_parts, '{}mm'.format(i), **arguments, gap = i)
 	
-	def regular_fillygon(file_name, sides, side_repetitions = 1, reversed_sides = ''):
-		fillygon(file_name, None, 'regular_fillygon', '{}, side_repetitions = {}, reversed_sides = [{}]'.format(sides, side_repetitions, reversed_sides))
+	def fillygon_filling(function, includes, *name_parts, **arguments):
+		fillygon_gap(function, includes, *name_parts, **arguments)
+		fillygon_gap(function, includes, *name_parts, 'filled', filled = True, **arguments)
 	
-	def non_regular_fillygon(file_name, included_file, reversed_sides = ''):
-		fillygon(file_name, included_file, 'fillygon', 'angles, reversed_sides = [{}]'.format(reversed_sides))
+	def regular_fillygon(sides, *name_parts, **arguments):
+		fillygon_filling('regular_fillygon', [], '{}-gon'.format(sides), *name_parts, **arguments, num_sides = sides)
+	
+	def non_regular_fillygon(name, *name_parts, **arguments):
+		fillygon_filling('fillygon', [('include', 'custom_angles/{}.scad'.format(name))], name, *name_parts, **arguments, angles = symbol('angles'))
 	
 	# Regular n-gons.
-	for i in range(3, 12):
-		regular_fillygon('{}-gon'.format(i), i, 1)
+	for i in range(3, 12 + 1):
+		regular_fillygon(i)
 	
 	# n-gons with reversed sides.
-	regular_fillygon('3-gon-reversed', 3, 1, 'true')
-	regular_fillygon('4-gon-reversed-1', 4, 1, 'true')
-	regular_fillygon('4-gon-reversed-2', 4, 1, 'true, true')
-	regular_fillygon('4-gon-reversed-3', 4, 1, 'true, false, true')
+	regular_fillygon(3, 'reversed', reversed_edges = [True])
+	regular_fillygon(4, 'reversed-1', reversed_edges = [True])
+	regular_fillygon(4, 'reversed-2', reversed_edges = [True, True])
+	regular_fillygon(4, 'reversed-3', reversed_edges = [True, False, True])
 	
 	# n-gons with doubled sides.
-	for i in range(3, 7):
-		regular_fillygon('{}-gon-double'.format(i), i, 2)
+	for i in range(3, 6 + 1):
+		regular_fillygon(i, 'double', side_repetitions = 2)
 	
 	# n-gons with custom angles.
 	for i in os.listdir('src/custom_angles'):
 		if i.startswith('_') and i.endswith('.scad'):
-			name = i[1:-5]
-			
-			non_regular_fillygon(name, 'custom_angles/_{}'.format(i))
+			non_regular_fillygon(i[1:-5])
 	
-	for i in files if path is None else files[path]():
+	for i in sorted(files) if path is None else files[path]():
 		print(i)
 
 
