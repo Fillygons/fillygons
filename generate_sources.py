7fd78ad1b69c66c62b59b91be973032a4d930d25
Michael Schwarz <michi.schwarz@gmail.com>
2016-02-04 01:42:40 +0100

    Function to generate angle list for regular fillygons.

--- generate_sources.py
+++ generate_sources.py
@@ -32,24 +32,27 @@ def main(path = None):
 	
 	files = { }
 	
-	def generate_file(function : list, *name_parts, **arguments):
+	def generate_file(name, expression):
 		current_includes = list(includes)
 		
 		def fn():
 			for type, path in current_includes:
 				yield '{} <{}>'.format(type, path)
 			
-			yield 'render() {};'.format(call(function, **arguments))
+			yield 'render() {};'.format(serialize_value(expression))
 		
-		path = 'src/{}.scad'.format('-'.join(name_parts))
+		path = 'src/{}.scad'.format(name)
 		
 		assert path not in files
 		
 		files[path] = fn
 	
-	def fillygon(function, *name_parts, **arguments):
+	def fillygon(*name_parts, **arguments):
 		with include('_fillygon.scad'):
-			generate_file(function, *name_parts, **arguments)
+			expression = call('fillygon', **arguments)
+			name = '-'.join(name_parts)
+			
+			generate_file(name, expression)
 	
 	def fillygon_gap(function, *name_parts, **arguments):
 		for i in .25, .4:
@@ -63,26 +66,26 @@ def main(path = None):
 		fillygon_corners(function, *name_parts, **arguments)
 		fillygon_corners(function, *name_parts, 'filled', filled = True, **arguments)
 	
-	def regular_fillygon(sides, *name_parts, **arguments):
-		fillygon_filling('regular_fillygon', '{}-gon'.format(sides), *name_parts, **arguments, num_sides = sides)
+	def regular_fillygon(sides, side_repetitions, *name_parts, **arguments):
+		fillygon_filling('{}-gon'.format(sides), *name_parts, **arguments, angles = call('regular_angles', num_sides = sides, side_repetitions = side_repetitions))
 	
 	def non_regular_fillygon(name, *name_parts, **arguments):
 		with include('custom_angles/_{}.scad'.format(name), 'use'):
-			fillygon_filling('fillygon', name, *name_parts, **arguments, angles = expression('angles'))
+			fillygon_filling(name, *name_parts, **arguments, angles = expression('angles'))
 	
 	# Regular n-gons.
 	for i in range(3, 12 + 1):
-		regular_fillygon(i)
+		regular_fillygon(i, 1)
 	
 	# n-gons with reversed sides.
-	regular_fillygon(3, 'reversed', reversed_edges = [True])
-	regular_fillygon(4, 'reversed-1', reversed_edges = [True])
-	regular_fillygon(4, 'reversed-2', reversed_edges = [True, True])
-	regular_fillygon(4, 'reversed-3', reversed_edges = [True, False, True])
+	regular_fillygon(3, 1, 'reversed', reversed_edges = [True])
+	regular_fillygon(4, 1, 'reversed-1', reversed_edges = [True])
+	regular_fillygon(4, 1, 'reversed-2', reversed_edges = [True, True])
+	regular_fillygon(4, 1, 'reversed-3', reversed_edges = [True, False, True])
 	
 	# n-gons with doubled sides.
 	for i in range(3, 6 + 1):
-		regular_fillygon(i, 'double', side_repetitions = 2)
+		regular_fillygon(i, 2, 'double')
 	
 	# n-gons with custom angles.
 	for i in os.listdir('src/custom_angles'):
