5c8087e255c0ce41ad914c5d5ad622f712006782
Raoul <raoul@ccczh.ch>
2018-01-05 18:56:37 +0100

    Add non-equilateral fillygons to generator

--- generate_sources/variants.py
+++ generate_sources/variants.py
@@ -24,102 +24,118 @@ def decide_file(decider: Decider):
     reversed_edges = []
     side_repetitions = 1
 
-    regular = decider.get_boolean()
+    equilateral = decider.get_boolean()
 
-    if regular:
-        if decider.get_boolean():
-            num_sides = decider.get_item(range(3, 12 + 1))
+    if equilateral:
 
-            if num_sides <= 6:
-                side_repetitions = decider.get(1, 2)
+        regular = decider.get_boolean()
+
+        if regular:
+            if decider.get_boolean():
+                num_sides = decider.get_item(range(3, 12 + 1))
+
+                if num_sides <= 6:
+                    side_repetitions = decider.get(1, 2)
+            else:
+                # n-gons with reversed sides.
+                num_sides, *reversed_edges = decider.get(
+                    (3, True),
+                    (4, True),
+                    (4, True, True),
+                    (4, True, False, True),
+                    (5, True),
+                    (5, True, True),
+                    (5, True, False, True))
+
+                reversed_edges += (False,) * (num_sides - len(reversed_edges))
+
+            directions = [
+                2 * pi / num_sides * i
+                for i in range(num_sides)
+                for _ in range(side_repetitions)]
+
+            angles = [pi - b + a for a, b in zip(directions, directions[1:])]
+
+            name = '{}-Gon'.format(num_sides)
+            polygon_name = '{}-gon'.format(num_sides)
+
+            if side_repetitions > 1:
+                polygon_name += '-double'
+
+            if reversed_edges:
+                polygon_name += '-reversed-{}'.format(''.join('.r'[i] for i in reversed_edges))
         else:
-            # n-gons with reversed sides.
-            num_sides, *reversed_edges = decider.get(
-                (3, True),
-                (4, True),
-                (4, True, True),
-                (4, True, False, True),
-                (5, True),
-                (5, True, True),
-                (5, True, False, True))
+            if decider.get_boolean():
+                # Rhombi
+                short_diagonal, long_diagonal = decider.get(
+                    # Diamond
+                    (1, sqrt(3)),
+                    # Rhombic Dodecahedron
+                    (1, sqrt(2)),
+                    # Rhombic Triacontahedron
+                    (1, GoldenRatio),
+                    # Rhombic Enneacontahedron
+                    (1, GoldenRatio ** 2),
 
-            reversed_edges += (False,) * (num_sides - len(reversed_edges))
+                    # Spiral Tube n = 4 and m = 1
+                    (sqrt(3), sqrt(5)),
 
-        directions = [
-            2 * pi / num_sides * i
-            for i in range(num_sides)
-            for _ in range(side_repetitions)]
+                    # Polar Zonohedron n = 5
+                    (sqrt(7 - sqrt(5)), sqrt(5 + sqrt(5))),
+                    (sqrt(5 - sqrt(5)), sqrt(7 + sqrt(5))),
+                    # Polar Zonohedron n = 6
+                    (1, sqrt(5)),
+                )
 
-        angles = [pi - b + a for a, b in zip(directions, directions[1:])]
+                assert short_diagonal < long_diagonal
 
-        name = '{}-Gon'.format(num_sides)
-        polygon_name = '{}-gon'.format(num_sides)
+                acute_angle = 2 * atan(S(short_diagonal) / long_diagonal)
+                degrees_rounded = round(float(deg(acute_angle)))
+                name = 'Rhombus ({})'.format(degrees_rounded)
+                polygon_name = 'rhombus-{}'.format(degrees_rounded)
 
-        if side_repetitions > 1:
-            polygon_name += '-double'
+                angles = [acute_angle, pi - acute_angle, acute_angle]
+            elif decider.get_boolean():
+                # Flat hexagons
+                opposite_angle = decider.get(
+                    2 * atan(GoldenRatio),
+                    pi / 2,
+                    2 * atan(sqrt(2)),
+                    2 * atan(1 / GoldenRatio),
+                    2 * atan(1 / sqrt(2)))
 
-        if reversed_edges:
-            polygon_name += '-reversed-{}'.format(''.join('.r'[i] for i in reversed_edges))
+                other_angle = pi - opposite_angle / 2
+
+                degrees_rounded = round(float(deg(opposite_angle)))
+                name = '6-Gon {}'.format(degrees_rounded)
+                polygon_name = '6-gon-flat-{}'.format(degrees_rounded)
+
+                angles = [
+                    other_angle,
+                    opposite_angle,
+                    other_angle,
+                    other_angle,
+                    opposite_angle]
+            else:
+                # Special tiles
+                name, polygon_name, *angles_degree = decider.get(
+                    ('Rectangle', 'rectangle', 180, 90, 90, 180, 90),
+                    ('Triamond', 'triamond', 60, 120, 120, 60))
+
+                angles = [rad(a) for a in angles_degree]
+
+        # Constant edge length for all equilateral polygons
+        edges = len(angles) * [1]
+
     else:
-        if decider.get_boolean():
-            # Rhombi
-            short_diagonal, long_diagonal = decider.get(
-                # Diamond
-                (1, sqrt(3)),
-                # Rhombic Dodecahedron
-                (1, sqrt(2)),
-                # Rhombic Triacontahedron
-                (1, GoldenRatio),
-                # Rhombic Enneacontahedron
-                (1, GoldenRatio ** 2),
+        # Non-equilateral polygons
+        regular = False
 
-                # Spiral Tube n = 4 and m = 1
-                (sqrt(3), sqrt(5)),
+        name, polygon_name, angles_degree, edges = decider.get(
+            ('RightIsosceleTriangle', 'right-isoscele-triangle', [90, 45], [1, 1, sqrt(2)]))
 
-                # Polar Zonohedron n = 5
-                (sqrt(7 - sqrt(5)), sqrt(5 + sqrt(5))),
-                (sqrt(5 - sqrt(5)), sqrt(7 + sqrt(5))),
-                # Polar Zonohedron n = 6
-                (1, sqrt(5)),
-            )
+        angles = [rad(a) for a in angles_degree]
 
-            assert short_diagonal < long_diagonal
-
-            acute_angle = 2 * atan(S(short_diagonal) / long_diagonal)
-            degrees_rounded = round(float(deg(acute_angle)))
-            name = 'Rhombus ({})'.format(degrees_rounded)
-            polygon_name = 'rhombus-{}'.format(degrees_rounded)
-
-            angles = [acute_angle, pi - acute_angle, acute_angle]
-        elif decider.get_boolean():
-            # Flat hexagons
-            opposite_angle = decider.get(
-                2 * atan(GoldenRatio),
-                pi / 2,
-                2 * atan(sqrt(2)),
-                2 * atan(1 / GoldenRatio),
-                2 * atan(1 / sqrt(2)))
-
-            other_angle = pi - opposite_angle / 2
-
-            degrees_rounded = round(float(deg(opposite_angle)))
-            name = '6-Gon {}'.format(degrees_rounded)
-            polygon_name = '6-gon-flat-{}'.format(degrees_rounded)
-
-            angles = [
-                other_angle,
-                opposite_angle,
-                other_angle,
-                other_angle,
-                opposite_angle]
-        else:
-            name, polygon_name, *angles_degree = decider.get(
-                ('Rectangle', 'rectangle', 180, 90, 90, 180, 90),
-                ('Triamond', 'triamond', 60, 120, 120, 60))
-
-            angles = [rad(a) for a in angles_degree]
-
-    edges = len(angles) * [1]
 
     angles.append((len(angles) - 1) * pi - sum(angles))
 
@@ -173,7 +189,6 @@ def decide_file(decider: Decider):
         angles_values=[float(a) for a in angles],
         edges_formulae=[latex(e, inv_trig_style='full') for e in edges],
         edges_values=[float(e) for e in edges],
-        edges=edges,
         reversed_edges=reversed_edges,
         filled=filled,
         filled_corners=filled_corners,
