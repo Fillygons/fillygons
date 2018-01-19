5a96affc7b895b59b048e6b35851a666cb2fe7d8
Michael Schwarz <michi.schwarz@gmail.com>
2018-01-19 21:49:59 +0100

    Move Python dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='fillygons',
     packages=['fillygons'],
+    install_requires=['sympy'],
     entry_points=dict(
         console_scripts=[
             'generate_sources = fillygons.generate_sources:script_main']))
